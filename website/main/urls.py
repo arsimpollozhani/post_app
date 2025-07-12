2e36ac6001ecac41564efb22ffa182acc082979f
Arsim Pollozhani <pollozhaniarsim@gmail.com>
2025-07-13 00:02:20 +0200

    add feature: edit post

--- website/main/urls.py
+++ website/main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('sign-up/', views.sign_up, name='sign_up'),
     path('create-post/', views.create_post, name='create_post'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
--- website/main/views.py
+++ website/main/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegisterForm, PostForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from django.urls import reverse_lazy, reverse
 # Create your views here.
 
 
-@login_required(login_url='/login')
+@login_required(login_url=reverse_lazy('login'))
 def home (request):
     posts = Post.objects.all()
 
@@ -25,24 +26,7 @@ def home (request):
     return render(request, 'main/home.html', context)
 
 
-def sign_up(request):
-    if request.method == 'POST':
-        form = RegisterForm (request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-    else:  # request.method == 'GET'
-        form = RegisterForm()
-    context = {
-        "form" : form,
-    }
-
-
-    return render (request, 'registration/sign_up.html', context)
-
-
-@login_required(login_url='/login')
+@login_required(login_url=reverse_lazy('login'))
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -58,4 +42,43 @@ def create_post(request):
         "form" : form,
     }
     return render (request, "main/create_post.html", context)
+
+
+@login_required(login_url=reverse_lazy('login'))
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if post.author != request.user:
+        return redirect('/home')
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('/home')
+    else: # GET
+        form = PostForm(instance=post)
+
+    # even defined in if-stat, form is available in function-level scope
+    context = {
+        "form" : form,
+        "post": post,
+    }
+    return render (request, 'main/edit_post.html', context)
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm (request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
+    else:  # request.method == 'GET'
+        form = RegisterForm()
+    context = {
+        "form" : form,
+    }
+    return render (request, 'registration/sign_up.html', context)
+
 
